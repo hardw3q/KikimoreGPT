f9907656b63387aca9b26a8e840ea7e3f11eea37
hardw3q <sob.dod@gmail.com>
2023-02-12 19:00:40 +0300

    f

---
+++ bot.py
@@ -0,0 +1,7 @@
+import openai
+import telegram
+from openai import  *
+from telegram import  *
+from telegram.ext import *
+
+bot = Bot("6156173044:AAFIugymGRv2SVLKsYC6rL5vfDUBjvodEOY")
---
+++ main.py
@@ -0,0 +1,26 @@
+import openai
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Set up OpenAI API key
+openai.api_key = "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"
+
+# Set up Telegram Bot
+updater = Updater(token="6156173044:AAFIugymGRv2SVLKsYC6rL5vfDUBjvodEOY", use_context=True)
+dispatcher = updater.dispatcher
+
+def chatbot(update, context):
+    message = update.message.text
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt='Response: ' + message,
+        max_tokens=4000,
+        stop=None,
+        temperature=0.5,
+    ).choices[0].text
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+chat_handler = MessageHandler(Filters.text, chatbot)
+dispatcher.add_handler(chat_handler)
+
+updater.start_polling()
